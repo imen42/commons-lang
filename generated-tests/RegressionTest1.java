841b1f1cf3cbf873b6287829e902fd4e078d9fdb
imen42 <yahyaimen971@gmail.com>
2025-02-13 22:10:22 +0100

    fix spotbugs bugs

--- generated-tests/RegressionTest1.java
+++ generated-tests/RegressionTest1.java
@@ -14,7 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
